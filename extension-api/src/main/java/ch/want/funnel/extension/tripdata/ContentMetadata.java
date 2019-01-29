c50de3f9d0a63a2aa34b8ddaa289dfff61073ca1
Simon Niederberger <simon.niederberger@want.ch>
2019-01-29 07:44:15 +0000

    Extended ContentMetadata with a MIME type

--- extension-api/src/main/java/ch/want/funnel/extension/tripdata/ContentMetadata.java
+++ extension-api/src/main/java/ch/want/funnel/extension/tripdata/ContentMetadata.java
@@ -10,17 +10,18 @@ public class ContentMetadata {
     public final OffsetDateTime creationDate;
     public final String description;
     public final String participants;
+    public final String mimeType;
 
-    public ContentMetadata(final OffsetDateTime creationDate, final String description, final String participants) {
-        super();
+    public ContentMetadata(final OffsetDateTime creationDate, final String mimeType, final String description, final String participants) {
         this.creationDate = creationDate;
+        this.mimeType = mimeType;
         this.description = description;
         this.participants = participants;
     }
 
     /**
      * Get the date this content was originally created. If unknown, OffsetDateTime.now() should be used.
-     * 
+     *
      * @return
      */
     public OffsetDateTime getCreationDate() {
@@ -29,7 +30,7 @@ public class ContentMetadata {
 
     /**
      * Returns a description of the content, such as an e-mail subject, a hotel name, or possible simply a PNR
-     * 
+     *
      * @return
      */
     public String getDescription() {
@@ -38,10 +39,24 @@ public class ContentMetadata {
 
     /**
      * Get a comma-separated list of participant names
-     * 
+     *
      * @return
      */
     public String getParticipants() {
         return participants;
+    }
+
+    /**
+     * MIME types handled by funnel.travel:
+     * - multipart/mixed (for MIME e-mail messages)
+     * - text/plain
+     * - text/html
+     * - application/json
+     * - application/xml
+     *
+     * @return
+     */
+    public String getMimeType() {
+        return mimeType;
     }
 }
