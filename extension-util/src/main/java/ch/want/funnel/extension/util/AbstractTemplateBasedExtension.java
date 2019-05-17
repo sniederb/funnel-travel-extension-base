71cc37e6290a9597731e36dc90dad06968c911cd
Simon Niederberger <simon.niederberger@want.ch>
2019-05-17 11:48:58 +0200

    Added methods to load ad-hoc templates

--- extension-util/src/main/java/ch/want/funnel/extension/util/AbstractTemplateBasedExtension.java
+++ extension-util/src/main/java/ch/want/funnel/extension/util/AbstractTemplateBasedExtension.java
@@ -4,9 +4,15 @@
 package ch.want.funnel.extension.util;
 
 import java.io.IOException;
+import java.io.StringReader;
 import java.io.StringWriter;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Collection;
 import java.util.Collections;
+import java.util.Date;
 import java.util.Map;
+import java.util.Optional;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
@@ -29,6 +35,7 @@ public abstract class AbstractTemplateBasedExtension {
     private static Configuration freemarkerConfig;
     private static final ObjectMapper OBJECTMAPPER = new ObjectMapper();
     private static final XmlMapper XMLMAPPER = new XmlMapper();
+    private static final String XML_DATE_PATTERN = "yyyy-MM-dd";
 
     protected Map<String, Object> parsePayloadToMap(final String payload) throws IOException {
         if (payload == null) {
@@ -40,6 +47,14 @@ public abstract class AbstractTemplateBasedExtension {
         return parseJsonPayloadToMap(payload);
     }
 
+    /**
+     * Load the provided JSON into a Java map. Beware that this method does NOT
+     * attempt and date/time conversion. Use {@link #convertPayloadDates(Map)} if value objects
+     * need to be dates.
+     *
+     * @param payloadNode
+     * @return
+     */
     protected Map<String, Object> parsePayloadToMap(final JsonNode payloadNode) {
         return OBJECTMAPPER.convertValue(payloadNode, new TypeReference<Map<String, Object>>() {
         });
@@ -56,6 +71,35 @@ public abstract class AbstractTemplateBasedExtension {
         });
     }
 
+    @SuppressWarnings("unchecked")
+    protected void convertPayloadDates(final Map<?, Object> payload) {
+        for (final Map.Entry<?, Object> entry : payload.entrySet()) {
+            if (entry.getValue() instanceof Map) {
+                convertPayloadDates((Map<?, Object>) entry.getValue());
+            } else if (entry.getValue() instanceof Collection<?>) {
+                final Collection<?> items = (Collection<?>) entry.getValue();
+                items.stream().forEach(item -> {
+                    if (item instanceof Map) {
+                        convertPayloadDates((Map<?, Object>) item);
+                    }
+                });
+            } else if (entry.getValue() instanceof String) {
+                getAsDate((String) entry.getValue()).ifPresent(entry::setValue);
+            }
+        }
+    }
+
+    private Optional<Date> getAsDate(final String candidate) {
+        if ((candidate != null) && (candidate.length() == XML_DATE_PATTERN.length()) && (candidate.charAt(4) == '-')) {
+            try {
+                return Optional.of(new SimpleDateFormat(XML_DATE_PATTERN).parse(candidate));
+            } catch (final ParseException ex) {
+                // no-op, apparently not a date
+            }
+        }
+        return Optional.empty();
+    }
+
     /**
      * Load a template by resource name. Note that the template is cached internally already, so there
      * is no need for caching on the caller side.
@@ -68,6 +112,19 @@ public abstract class AbstractTemplateBasedExtension {
     protected Template loadTemplate(final String templateResourceName) throws IOException {
         assertConfigurationPublished();
         return freemarkerConfig.getTemplate(templateResourceName);
+    }
+
+    /**
+     * Parse a simple template from a string. Note that this method does NOT support template referencing.
+     *
+     * @param templateResourceName
+     *            The full resource name, ie. incl. package path (even if template is in the same package as the caller)
+     * @return
+     * @throws IOException
+     */
+    protected Template parseTemplate(final String template) throws IOException {
+        assertConfigurationPublished();
+        return new Template("adhoc-" + template.hashCode(), new StringReader(template), freemarkerConfig);
     }
 
     private void assertConfigurationPublished() {
