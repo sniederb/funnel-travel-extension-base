f8750871b98f4bd1d6c4d0d16d86cd83b3eada7a
Simon Niederberger <simon.niederberger@want.ch>
2019-11-14 11:48:45 +0100

    Added util method to access PNR remark data

--- extension-util/src/main/java/ch/want/funnel/extension/util/ExtensionDataAccess.java
+++ extension-util/src/main/java/ch/want/funnel/extension/util/ExtensionDataAccess.java
@@ -1,12 +1,17 @@
 package ch.want.funnel.extension.util;
 
+import java.util.Iterator;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import ch.want.funnel.extension.model.Booking;
+import ch.want.funnel.extension.model.Trip;
 
 public class ExtensionDataAccess {
 
@@ -16,10 +21,44 @@ public class ExtensionDataAccess {
         this.booking = booking;
     }
 
+    /**
+     * Search in all extension data for a 'remarks' array, and therein an entry with the provided {@code remarkPrefix}.
+     * If found, the entry value string is returned.
+     */
+    public static Optional<String> searchAllExtensionDataForRemark(final Trip trip, final String remarkPrefix) {
+        final List<JsonNode> allExtensionData = trip.getBookings().stream()
+            .filter(bk -> bk.getExtensionData() != null)
+            .map(Booking::getExtensionData)
+            .collect(Collectors.toList());
+        for (final JsonNode extensionData : allExtensionData) {
+            for (final Iterator<String> iterator = extensionData.fieldNames(); iterator.hasNext();) {
+                final String extensionClassname = iterator.next();
+                final Optional<String> result = searchExtensionSubnode(extensionData.get(extensionClassname), remarkPrefix);
+                if (result.isPresent()) {
+                    return result;
+                }
+            }
+        }
+        return Optional.empty();
+    }
+
+    private static Optional<String> searchExtensionSubnode(final JsonNode extensionSubnode, final String remarkPrefix) {
+        final JsonNode remarks = extensionSubnode.get("remarks");
+        if ((remarks != null) && remarks.isArray()) {
+            final ArrayNode remarksArray = (ArrayNode) remarks;
+            for (final JsonNode value : remarksArray) {
+                if (!value.isNull() && value.asText().startsWith(remarkPrefix)) {
+                    return Optional.of(value.asText());
+                }
+            }
+        }
+        return Optional.empty();
+    }
+
     public Optional<JsonNode> getNode(final String extensionClassname, final String key) {
         return Optional.ofNullable(booking.getExtensionData())
-                .map(json -> json.get(extensionClassname))
-                .map(node -> node.get(key));
+            .map(json -> json.get(extensionClassname))
+            .map(node -> node.get(key));
     }
 
     /**
@@ -32,11 +71,11 @@ public class ExtensionDataAccess {
      */
     public JsonNode getOrCreateNode(final String extensionClassname, final String key, final boolean array) {
         return getNode(extensionClassname, key)
-                .orElseGet(() -> {
-                    final JsonNode newNode = array ? JsonNodeFactory.instance.arrayNode() : JsonNodeFactory.instance.objectNode();
-                    setNode(extensionClassname, key, newNode);
-                    return newNode;
-                });
+            .orElseGet(() -> {
+                final JsonNode newNode = array ? JsonNodeFactory.instance.arrayNode() : JsonNodeFactory.instance.objectNode();
+                setNode(extensionClassname, key, newNode);
+                return newNode;
+            });
     }
 
     public ExtensionDataAccess setNode(final String extensionClassname, final String key, final JsonNode node) {
