b7506318ad40c1e49ac2c6e5faa3db77566d0b7d
Simon Niederberger <simon.niederberger@want.ch>
2021-03-13 18:02:52 +0100

    Improved parsing of GDS pax strings

--- extension-util/src/main/java/ch/want/funnel/extension/util/GdsPassengerName.java
+++ extension-util/src/main/java/ch/want/funnel/extension/util/GdsPassengerName.java
@@ -1,13 +1,62 @@
 package ch.want.funnel.extension.util;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.util.HashSet;
+import java.util.Locale;
+import java.util.Set;
+
 import org.apache.commons.lang3.StringUtils;
 
 public class GdsPassengerName {
 
+    private static final Set<String> MALE_SALUATIONS;
+    private static final Set<String> FEMALE_SALUATIONS;
     private final String lastname;
     private final String firstname;
-    private final String title;
-    private final String salutation;
+    private String title;
+    private String salutation;
+    private String passengerType;
+    private String passengerId;
+    private String infantName;
+    private LocalDate infantBirthdate;
+    static {
+        MALE_SALUATIONS = new HashSet<>();
+        MALE_SALUATIONS.add("MR");
+        MALE_SALUATIONS.add("MR.");
+        FEMALE_SALUATIONS = new HashSet<>();
+        FEMALE_SALUATIONS.add("MRS");
+        FEMALE_SALUATIONS.add("MRS.");
+        FEMALE_SALUATIONS.add("MS");
+        FEMALE_SALUATIONS.add("MS.");
+        FEMALE_SALUATIONS.add("MISS");
+    }
+
+    /**
+     * GDS will use salutations MR, MRS, CHD and INF, while other booking sources might use
+     * Mr., Ms., Mrs., MISS, MS etc. This method returns a standard set of saluations:
+     * <ul>
+     * <li>'MRS' for any female-type salutations</li>
+     * <li>'MR' for any male-type salutation</li>
+     * <li>An empty string for unknown salutations, or those indicating age instead of gender</li>
+     * </ul>
+     *
+     * @param salutation
+     * @return
+     */
+    public static String normalizeSalutation(final String salutation) {
+        if (salutation == null) {
+            return null;
+        }
+        if (MALE_SALUATIONS.contains(salutation.toUpperCase())) {
+            return "MR";
+        }
+        if (FEMALE_SALUATIONS.contains(salutation.toUpperCase())) {
+            return "MRS";
+        }
+        return "";
+    }
 
     /**
      * Parse a complete name as typically sent by a GDS. eg.
@@ -28,24 +77,10 @@ public class GdsPassengerName {
         }
         this.lastname = fullname.substring(0, firstSlash);
         String remainingComposite = fullname.substring(firstSlash + 1);
-        this.salutation = getSuffix(remainingComposite, "MR", "MRS", "MS");
-        if (StringUtils.isNotBlank(salutation)) {
-            remainingComposite = remainingComposite.substring(0, remainingComposite.length() - this.salutation.length()).trim();
-        }
-        this.title = getSuffix(remainingComposite, "DR", "PROF");
-        if (StringUtils.isNotBlank(title)) {
-            remainingComposite = remainingComposite.substring(0, remainingComposite.length() - this.title.length()).trim();
-        }
+        remainingComposite = extractAdditionalInformation(remainingComposite);
+        remainingComposite = extractSalutation(remainingComposite);
+        remainingComposite = extractTitle(remainingComposite);
         this.firstname = remainingComposite;
-    }
-
-    private String getSuffix(final String s, final String... possibleSuffixes) {
-        for (final String suffix : possibleSuffixes) {
-            if (s.endsWith(suffix)) {
-                return suffix;
-            }
-        }
-        return "";
     }
 
     public String getLastname() {
@@ -62,5 +97,127 @@ public class GdsPassengerName {
 
     public String getSalutation() {
         return salutation;
+    }
+
+    public String getPassengerType() {
+        return passengerType;
+    }
+
+    public String getPassengerId() {
+        return passengerId;
+    }
+
+    public String getInfantName() {
+        return infantName;
+    }
+
+    public LocalDate getInfantBirthdate() {
+        return infantBirthdate;
+    }
+
+    private String extractAdditionalInformation(final String s) {
+        String remainingComposite = s;
+        int lastBracket = remainingComposite.lastIndexOf('(');
+        while (lastBracket >= 0) {
+            final PassengerNameAddOn addOn = new PassengerNameAddOn(remainingComposite.substring(lastBracket));
+            switch (addOn.getAddOnType()) {
+            case "ID":
+                this.passengerId = addOn.getFirstValueElement();
+                break;
+            case "INF":
+                this.infantName = addOn.getFirstValueElement();
+                this.infantBirthdate = addOn.getBirthdate();
+                break;
+            case "CHD":
+            case "ADT":
+                this.passengerType = addOn.getAddOnType();
+                break;
+            }
+            remainingComposite = removeTrailing(remainingComposite, addOn.getEntireAddOn());
+            lastBracket = remainingComposite.lastIndexOf('(');
+        }
+        return remainingComposite;
+    }
+
+    private String extractSalutation(final String s) {
+        this.salutation = getSuffix(s, "MR", "MRS", "MS");
+        if (StringUtils.isNotBlank(salutation)) {
+            return removeTrailing(s, this.salutation);
+        }
+        return s;
+    }
+
+    private String extractTitle(final String s) {
+        this.title = getSuffix(s, "DR", "PROF");
+        if (StringUtils.isNotBlank(title)) {
+            return removeTrailing(s, this.title);
+        }
+        return s;
+    }
+
+    private String getSuffix(final String s, final String... possibleSuffixes) {
+        for (final String suffix : possibleSuffixes) {
+            if (s.endsWith(suffix)) {
+                return suffix;
+            }
+        }
+        return "";
+    }
+
+    private String removeTrailing(final String base, final String suffix) {
+        return base.substring(0, base.length() - suffix.length()).trim();
+    }
+
+    private static class PassengerNameAddOn {
+
+        private static final DateTimeFormatter DDMMMYY = new DateTimeFormatterBuilder()
+            .parseCaseInsensitive()
+            .appendPattern("ddMMMyy")
+            .toFormatter(Locale.US);
+        private final String addOn;
+        private final String addOnType;
+        private final String firstValueElement;
+        private LocalDate birthdate;
+
+        PassengerNameAddOn(final String addOn) {
+            this.addOn = addOn;
+            final String payload = addOn.replaceAll("[\\(\\)]", "");
+            if (payload.startsWith("ID")) {
+                addOnType = "ID";
+                firstValueElement = payload.substring(2);
+                birthdate = null;
+            } else {
+                final String[] parts = payload.split("/");
+                addOnType = parts[0];
+                firstValueElement = parts.length > 1 ? parts[1] : "";
+                birthdate = parts.length > 2 ? parseBirthdate(parts[2]) : null;
+            }
+        }
+
+        /**
+         * Format: 04OCT10
+         * 
+         * @param dateValue
+         * @return
+         */
+        private LocalDate parseBirthdate(final String dateValue) {
+            return LocalDate.parse(dateValue, DDMMMYY);
+        }
+
+        String getEntireAddOn() {
+            return addOn;
+        }
+
+        String getAddOnType() {
+            return addOnType;
+        }
+
+        LocalDate getBirthdate() {
+            return birthdate;
+        }
+
+        String getFirstValueElement() {
+            return firstValueElement;
+        }
     }
 }
--- extension-util/src/test/java/ch/want/funnel/extension/util/GdsPassengerNameTest.java
+++ extension-util/src/test/java/ch/want/funnel/extension/util/GdsPassengerNameTest.java
@@ -3,6 +3,7 @@ package ch.want.funnel.extension.util;
 import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -23,10 +24,21 @@ public class GdsPassengerNameTest {
     public static Stream<Arguments> createTestdata() {
         return Stream.of(
             Arguments.of("NIEDERBERGER/SIMON MR", "NIEDERBERGER", "SIMON", "MR", ""),
-            Arguments.of("NIEDERBERGER/SIMONE MRS", "NIEDERBERGER", "SIMONE", "MRS", ""),
+            Arguments.of("NIEDERBERGER/SIMONE MRS(ID234567)", "NIEDERBERGER", "SIMONE", "MRS", ""),
             Arguments.of("NIEDERBERGER/SIMON DR MR", "NIEDERBERGER", "SIMON", "MR", "DR"),
             Arguments.of("NIEDERBERGER/SIMON PROF MR", "NIEDERBERGER", "SIMON", "MR", "PROF"),
             Arguments.of("NIEDERBERGER/SIMONMR", "NIEDERBERGER", "SIMON", "MR", ""),
-            Arguments.of("DOE/JOHN ALEXANDER MR", "DOE", "JOHN ALEXANDER", "MR", ""));
+            Arguments.of("DOE/JOHN ALEXANDER MR", "DOE", "JOHN ALEXANDER", "MR", ""),
+            Arguments.of("SIERRA/THALIA(CHD)", "SIERRA", "THALIA", "", ""),
+            Arguments.of("STRUMPF/MARGE MRS (INF/LENNY/04OCT10))", "STRUMPF", "MARGE", "MRS", ""),
+            Arguments.of("CARRINGTON/ALEXISMRS(ADT)(ID234567)", "CARRINGTON", "ALEXIS", "MRS", ""));
+    }
+
+    @Test
+    void parseInfant() {
+        final GdsPassengerName testee = new GdsPassengerName("STRUMPF/MARGE MRS (INF/LENNY/04OCT10))");
+        Assertions.assertEquals("LENNY", testee.getInfantName());
+        Assertions.assertNotNull(testee.getInfantBirthdate());
+        Assertions.assertEquals("2010-10-04", testee.getInfantBirthdate().toString());
     }
 }
