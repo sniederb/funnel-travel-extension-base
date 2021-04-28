4cec4f8b785f711756a642f1d627e69524a8bd2a
Simon Niederberger <simon.niederberger@want.ch>
2021-04-28 11:59:00 +0200

    Improved 'KeyNames' for easier creation of UI feedback

--- extension-api/src/main/java/ch/want/funnel/extension/KeyNames.java
+++ extension-api/src/main/java/ch/want/funnel/extension/KeyNames.java
@@ -1,17 +1,27 @@
-/*
- * Created on 21 Jan 2019
- */
 package ch.want.funnel.extension;
 
+/**
+ * This class defines constants used to add extension data which eventually will be displayed in funnel.travel.
+ * For this the extension must create an array with key {@link #ARRAYNODE_FOR_UI_ELEMENTS}, then add objects
+ * with the provided {@code UI_ELEMENT_xxx} keys.
+ */
 public final class KeyNames {
 
     public static final String ARRAYNODE_FOR_UI_ELEMENTS = "ui";
     public static final String UI_ELEMENT_CONTEXTTYPE = "contextType";
     public static final String UI_ELEMENT_CONTEXTUUID = "contextUuid";
+    /**
+     * funnel.travel will try to translate the value for this key using its internal messages. Most likely,
+     * this property should remain empty, and the entire message string should be saved in {@link #UI_ELEMENT_VALUE}
+     */
     public static final String UI_ELEMENT_LABELKEY = "labelKey";
     public static final String UI_ELEMENT_VALUETYPE = "valueType";
     public static final String UI_ELEMENT_VALUE = "value";
 
     private KeyNames() {
+    }
+
+    public enum ContextType {
+        BOOKING, SERVICE, LEG, SEGMENT, TRAVELER
     }
 }
