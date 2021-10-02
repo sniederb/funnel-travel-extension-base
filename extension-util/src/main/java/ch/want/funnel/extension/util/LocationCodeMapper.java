3c03d386c35969dec1bdfc12f7c3125bbd69fec9
Simon Niederberger <simon.niederberger@want.ch>
2021-10-02 09:07:56 +0100

    Added DB, more SNCF railstations

--- extension-util/src/main/java/ch/want/funnel/extension/util/LocationCodeMapper.java
+++ extension-util/src/main/java/ch/want/funnel/extension/util/LocationCodeMapper.java
@@ -5,6 +5,8 @@ import java.util.Optional;
 import java.util.PropertyResourceBundle;
 import java.util.ResourceBundle;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class LocationCodeMapper {
 
     private static final LocationCodeMapper instance = new LocationCodeMapper();
@@ -15,11 +17,14 @@ public class LocationCodeMapper {
     }
 
     /**
-     * SNCF maintains a list of train stations with codes which do not match the UN location codes.
-     * This method maps a SNCF train station code (eg. FRMSC) to the UN location code (eg. MRS/FR)
+     * Returns true if the {@code system} indicates to be part of the "Benerail/SNCF/Thalys" group. If this method returns true,
+     * use {@link #convertSncfToFunnelLocation(String)} for location codes.
      */
-    public static Optional<String> convertSncfToFunnelLocation(final String gareSncfCode) {
-        return instance.getMessage("sncf." + gareSncfCode);
+    public static boolean isSncfGroup(final String system) {
+        if (system == null) {
+            return false;
+        }
+        return StringUtils.containsAny(system.toLowerCase(), "bene", "sncf", "thalys");
     }
 
     /**
@@ -27,7 +32,32 @@ public class LocationCodeMapper {
      * This method maps an AMTRAK train station code (eg. ARB) to the UN location code (eg. ARB/US)
      */
     public static Optional<String> convertAmtrakToFunnelLocation(final String amtrakStationCode) {
+        if (StringUtils.isBlank(amtrakStationCode)) {
+            return Optional.empty();
+        }
         return instance.getMessage("amtrak." + amtrakStationCode);
+    }
+
+    /**
+     * DB (Deutsche Bahn) maintains a list of train stations with proprietary codes (RL100). This method maps a DB train station code (eg.
+     * XSZH) to the UN location code (eg. ZRH/CH)
+     */
+    public static Optional<String> convertDeutscheBahnToFunnelLocation(final String dbRl100Code) {
+        if (StringUtils.isBlank(dbRl100Code)) {
+            return Optional.empty();
+        }
+        return instance.getMessage("deutschebahn." + dbRl100Code.replace(' ', '_'));
+    }
+
+    /**
+     * SNCF maintains a list of train stations with codes which do not match the UN location codes.
+     * This method maps a SNCF train station code (eg. FRMSC) to the UN location code (eg. MRS/FR)
+     */
+    public static Optional<String> convertSncfToFunnelLocation(final String gareSncfCode) {
+        if (StringUtils.isBlank(gareSncfCode)) {
+            return Optional.empty();
+        }
+        return instance.getMessage("sncf." + gareSncfCode);
     }
 
     private Optional<String> getMessage(final String gareSncfCode) {
--- extension-util/src/test/java/ch/want/funnel/extension/util/LocationCodeMapperTest.java
+++ extension-util/src/test/java/ch/want/funnel/extension/util/LocationCodeMapperTest.java
@@ -6,14 +6,34 @@ import java.util.stream.Stream;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
 public class LocationCodeMapperTest {
+
+    @ParameterizedTest()
+    @CsvSource({ "BENE,true", "BENERAIL,true", "SNCF,true", "Thalys,true", "DB,false", ",false" })
+    void convertSncfToFunnelLocation(final String system, final boolean expectedResult) {
+        final boolean result = LocationCodeMapper.isSncfGroup(system);
+        Assertions.assertEquals(expectedResult, result);
+    }
 
     @ParameterizedTest(name = "parseSncf_{0}")
     @MethodSource("createSncfTestdata")
     void convertSncfToFunnelLocation(final String sncfCode, final String expectedFunnelLocation) {
         final Optional<String> locationCode = LocationCodeMapper.convertSncfToFunnelLocation(sncfCode);
+        if (expectedFunnelLocation != null) {
+            Assertions.assertTrue(locationCode.isPresent());
+            Assertions.assertEquals(expectedFunnelLocation, locationCode.get());
+        } else {
+            Assertions.assertFalse(locationCode.isPresent());
+        }
+    }
+
+    @ParameterizedTest(name = "parseDeutscheBahn_{0}")
+    @MethodSource("createDbTestdata")
+    void convertDeutscheBahnToFunnelLocation(final String dbRl100Code, final String expectedFunnelLocation) {
+        final Optional<String> locationCode = LocationCodeMapper.convertDeutscheBahnToFunnelLocation(dbRl100Code);
         if (expectedFunnelLocation != null) {
             Assertions.assertTrue(locationCode.isPresent());
             Assertions.assertEquals(expectedFunnelLocation, locationCode.get());
@@ -37,6 +57,14 @@ public class LocationCodeMapperTest {
     public static Stream<Arguments> createSncfTestdata() {
         return Stream.of(
             Arguments.of("FRXSY", "SET/FR"),
+            Arguments.of(null, null),
+            Arguments.of("FRZZZ", null));
+    }
+
+    public static Stream<Arguments> createDbTestdata() {
+        return Stream.of(
+            Arguments.of("XSZH", "ZRH/CH"),
+            Arguments.of("WH  W", "HAW/DE"),
             Arguments.of(null, null),
             Arguments.of("FRZZZ", null));
     }
