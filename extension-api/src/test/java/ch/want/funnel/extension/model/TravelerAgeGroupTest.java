c227ccde001a161ce65ee4eb4018bfe42f9d5588
Simon Niederberger <simon.niederberger@want.ch>
2022-09-28 12:31:22 +0200

    Fix TravelerAgeGroupTest

--- extension-api/src/test/java/ch/want/funnel/extension/model/TravelerAgeGroupTest.java
+++ extension-api/src/test/java/ch/want/funnel/extension/model/TravelerAgeGroupTest.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.params.provider.CsvSource;
 class TravelerAgeGroupTest {
 
     @ParameterizedTest
-    @CsvSource({ "2022-01-05,2022-08-23,INFANT", "2019-01-05,2022-08-23,CHILD", "2009-01-05,2022-08-23,ADULT" })
+    @CsvSource({ "2022-01-05,2022-08-23,INF", "2019-01-05,2022-08-23,CHD", "2009-01-05,2022-08-23,ADT" })
     void fromBirthdate(final LocalDate birthDate, final LocalDate travelDate, final TravelerAgeGroup expected) {
         final TravelerAgeGroup actual = TravelerAgeGroup.fromBirthdate(birthDate, travelDate);
         Assertions.assertEquals(expected, actual);
