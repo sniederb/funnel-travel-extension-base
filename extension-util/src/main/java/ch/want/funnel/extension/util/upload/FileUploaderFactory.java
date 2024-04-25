3bb1272ebb68d9c1d819437a3b13a30f9a31d40f
Simon Niederberger <simon.niederberger@want.ch>
2024-04-25 16:28:21 +0100

    Added buildTargetFilename() method

--- extension-util/src/main/java/ch/want/funnel/extension/util/upload/FileUploaderFactory.java
+++ extension-util/src/main/java/ch/want/funnel/extension/util/upload/FileUploaderFactory.java
@@ -1,6 +1,12 @@
 package ch.want.funnel.extension.util.upload;
 
 import java.net.URI;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+import ch.want.funnel.extension.model.Booking;
 
 public class FileUploaderFactory {
 
@@ -22,5 +28,35 @@ public class FileUploaderFactory {
             return new HttpsUploader(resourceIdentifier, username, passwd);
         }
         throw new IllegalArgumentException("Don't know how to send/upload to " + resourceIdentifier);
+    }
+
+    /**
+     * Build a filename assuming something like
+     *
+     * <pre>
+     * trip-dump${yyyyMMdd}
+     * </pre>
+     *
+     * Valid placeholder are:
+     * <ul>
+     * <li>${refNumber}</li>
+     * <li>${uuid}</li>
+     * <li>any other ${...} placeholder will be interpreted as date format</li>
+     * </ul>
+     *
+     * @return
+     */
+    public static String buildTargetFilename(final Booking booking, final String targetFilePattern) {
+        String result = targetFilePattern
+            .replace("${refNumber}", booking.getReferenceNumber())
+            .replace("${uuid}", booking.getUuid().toString());
+        final int datePatternIndex = result.indexOf("${");
+        if (datePatternIndex >= 0) {
+            final String datePattern = result.substring(datePatternIndex + 2, result.indexOf('}', datePatternIndex));
+            final DateFormat dateFormat = new SimpleDateFormat(datePattern);
+            dateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
+            result = result.replace("${" + datePattern + "}", dateFormat.format(new Date()));
+        }
+        return result;
     }
 }
