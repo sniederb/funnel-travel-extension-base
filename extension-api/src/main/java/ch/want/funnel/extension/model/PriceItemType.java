c1655016727a404e42f38d943f49f8213085e19d
Simon Niederberger <simon.niederberger@want.ch>
2024-12-21 08:36:25 +0000

    Added surcharge priceitem types

--- extension-api/src/main/java/ch/want/funnel/extension/model/PriceItemType.java
+++ extension-api/src/main/java/ch/want/funnel/extension/model/PriceItemType.java
@@ -23,16 +23,26 @@ public enum PriceItemType {
      * {@link TransportDocument}, which itself has a {@link TransportDocument#getTraveler()} set
      */
     AUX_PER_PAX, //
+    /**
+     * General-purpose pricing, behaves identical to {@link #DEFAULT}.
+     */
     AUX_PER_BOOKING, //
     /**
-     * Booking fee. {@link PriceItem}s of this type attached to a {@link TransportDocument} are often merged with {@link #FARE_PER_PAX} by
-     * the
-     * consumer.
+     * Booking fee like a penalty fee or a ticketing fee. {@link PriceItem}s of this type attached to a {@link TransportDocument} are often
+     * merged with {@link #FARE_PER_PAX} by the consumer.
      */
     FEE_PER_PAX,
     /**
      * Indicates that this {@link PriceItem} will need to be paid on-site, by the customer. Midoffices typically will not include these
      * prices in the revenue stream, but might want to show the information to the customer.
      */
-    ONSITE
+    ONSITE,
+    /**
+     * Surcharge per pax. {@link PriceItem}s of this type must have a {@link PriceItem#getPurchasePrice()} of zero.
+     */
+    SURCHARGE_PER_PAX,
+    /**
+     * Surcharge per booking. {@link PriceItem}s of this type must have a {@link PriceItem#getPurchasePrice()} of zero.
+     */
+    SURCHARGE_PER_BOOKING
 }
