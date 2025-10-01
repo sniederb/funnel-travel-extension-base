5accba33e66be26104dd92b160413026e5409426
Simon Niederberger <simon.niederberger@want.ch>
2025-10-01 16:30:56 +0200

    Added further Payment Service Provider

--- extension-api/src/main/java/ch/want/funnel/extension/model/FormOfPaymentSystem.java
+++ extension-api/src/main/java/ch/want/funnel/extension/model/FormOfPaymentSystem.java
@@ -1,5 +1,35 @@
 package ch.want.funnel.extension.model;
 
+/**
+ * This enum lists payment processors and/or acquirers.
+ */
 public enum FormOfPaymentSystem {
-    DATATRANS, OTHER
+    /**
+     * https://www.datatrans.ch/
+     */
+    DATATRANS,
+    /**
+     * Fallback entry
+     */
+    OTHER,
+    /**
+     * https://www.computop.com/, owned by Nexi
+     */
+    COMPUTOP,
+    /**
+     * https://www.worldpay.com/
+     */
+    WORLDPAY,
+    /**
+     * https://www.stripe.com/
+     */
+    STRIPE,
+    /**
+     * https://www.paypal.com/
+     */
+    PAYPAL,
+    /**
+     * https://www.adyen.com/
+     */
+    ADYEN
 }
