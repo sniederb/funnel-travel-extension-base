348afd8acb78d5c5ab6bd540cb4d0a0c1b006568
Simon Niederberger <simon.niederberger@want.ch>
2025-10-10 08:49:15 +0200

    Added ACRISS pseudo-codes

--- extension-util/src/main/java/ch/want/funnel/extension/util/AcrissClassification.java
+++ extension-util/src/main/java/ch/want/funnel/extension/util/AcrissClassification.java
@@ -1,5 +1,9 @@
 package ch.want.funnel.extension.util;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
 import org.apache.commons.lang3.Validate;
 
 /**
@@ -9,12 +13,66 @@ import org.apache.commons.lang3.Validate;
  */
 public class AcrissClassification {
 
+    private static final Map<String, String> PSEUDO_CODES = new HashMap<>();
     private final String acrissCode;
+    static {
+        PSEUDO_CODES.put("ALLB", "2-3 Door");
+        PSEUDO_CODES.put("ALLC", "2 or 4 Door");
+        PSEUDO_CODES.put("ALLD", "4-5 Door");
+        PSEUDO_CODES.put("AELT", "Any Elite");
+        PSEUDO_CODES.put("ACPR", "Couple/Roadster");
+        PSEUDO_CODES.put("ASPC", "Non-Standard Fleet");
+        PSEUDO_CODES.put("APUP", "Any Pickup");
+        PSEUDO_CODES.put("AWGN", "Any Estate/Wagon");
+        PSEUDO_CODES.put("AREC", "Camper");
+        PSEUDO_CODES.put("ASUV", "Any SUV");
+        PSEUDO_CODES.put("AVAN", "Passenger Van");
+        PSEUDO_CODES.put("ASIX", "Any 6+ Pax Van");
+        PSEUDO_CODES.put("ASEV", "Any 7+ Pax van");
+        PSEUDO_CODES.put("AEIG", "Any 8-Pax+ Van");
+        PSEUDO_CODES.put("ANIN", "Any 9-Pax+ Van");
+        PSEUDO_CODES.put("AFWD", "Any 4WD/ AWD");
+        PSEUDO_CODES.put("ATRV", "All Terrain");
+        PSEUDO_CODES.put("ACGO", "Commercial Truck");
+        PSEUDO_CODES.put("ALMO", "Limousine");
+        PSEUDO_CODES.put("ASPT", "Sport");
+        PSEUDO_CODES.put("ACNV", "Convertible");
+        PSEUDO_CODES.put("AOFR", "Special Offer Car");
+        PSEUDO_CODES.put("AMNO", "Monospace");
+        PSEUDO_CODES.put("AMTO", "Motor Home");
+        PSEUDO_CODES.put("AMCY", "2-Wheel Vehicle");
+        PSEUDO_CODES.put("ACRS", "Crossover");
+        PSEUDO_CODES.put("AMAN", "All Manual Transmission");
+        PSEUDO_CODES.put("AUTO", "All Automatic Transmission");
+        PSEUDO_CODES.put("APET", "All Petrol Powered Vehicles");
+        PSEUDO_CODES.put("ADSL", "All Diesel Powered Vehicles");
+        PSEUDO_CODES.put("AGRN", "Any Lower Emission Vehicles (Hybrid, Electric, LPG, Hydrogen, Ethanol, Multi Fuel)");
+        PSEUDO_CODES.put("AHYB", "All Hybrid Vehicles");
+        PSEUDO_CODES.put("AELC", "All Electric Powered Vehicles");
+        PSEUDO_CODES.put("AHYD", "All Hydrogen Powered Vehicles");
+        PSEUDO_CODES.put("AMFP", "All Multi Fuel Powered Vehicles");
+        PSEUDO_CODES.put("ACPG", "All LPG/Compressed Gas – Powered Vehicles");
+        PSEUDO_CODES.put("AETH", "All Ethanol – Powered Vehicles");
+    }
 
     public AcrissClassification(final String acrissCode) {
         Validate.notNull(acrissCode);
         Validate.isTrue(acrissCode.length() == 4);
         this.acrissCode = acrissCode;
+    }
+
+    /**
+     * Check an ACRISS code against the list at https://www.acriss.org/car-codes/expanded-matrix/pseudo-car-codes/ .
+     */
+    public boolean isPseudoCode(final String acrissCode) {
+        return PSEUDO_CODES.containsKey(acrissCode);
+    }
+
+    /**
+     * Get a vehicle description for an ACRISS pseudo-code.
+     */
+    public Optional<String> getPseudoDefinition(final String acrissCode) {
+        return Optional.ofNullable(PSEUDO_CODES.get(acrissCode));
     }
 
     public String getCategory() {
@@ -95,7 +153,7 @@ public class AcrissClassification {
         case 'M':
             return "Monospace";
         case 'R':
-            return "Recreational Vehicle"; // motorhome or a smaller campervan
+            return "Recreational Vehicle";
         case 'H':
             return "Motor Home";
         case 'Y':
