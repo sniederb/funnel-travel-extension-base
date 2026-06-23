0c5f92f58dcbef1d34bf3a8eef83eff004468ec0
Lea A <lea.andrusz@solivagant.co.uk>
2026-06-23 15:15:31 +0100

    Add a simple Dictionary to start

---
+++ extension-api/src/main/java/ch/want/funnel/extension/Dictionary.java
@@ -0,0 +1,13 @@
+package ch.want.funnel.extension;
+
+public class Dictionary {
+    static final String CAPTION = "Caption";
+    static final String COMMISSION = "Commission";
+    static final String DISCOUNT = "Discount";
+    static final String FARE = "Fare";
+    static final String OFF_TICKET_FEE = "Off-ticket fee";
+    static final String PROMOTION = "Promotion";
+    static final String REFUND = "Refund";
+    static final String TAX = "Tax";
+    static final String TOTAL = "Total";
+}
